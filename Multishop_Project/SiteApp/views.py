0a2964e8357acc651ca778f91510688d4beba53d
USAM VIJAY <81009057+usamvijay@users.noreply.github.com>
2022-09-15 23:50:39 +0530

    Templates Creating

--- Multishop_Project/SiteApp/views.py
+++ Multishop_Project/SiteApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "Site/index.html")
+
+def cart_page(request):
+    return render(request, "Site/cart.html")
+
+def checkout_page(request):
+    return render(request, "Site/checkout.html")
